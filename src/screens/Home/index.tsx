7b2f9d6c677ceb12b1f6d3b69b5e76079358575d
Vitor de Paula <vitordcode@gmail.com>
2022-12-26 19:10:54 -0300

    Create component Participant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,21 +1,37 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
-import { styles } from './styles'
+import Participant from "../../components/Participant";
+
+import { styles } from "./styles";
 
 export default function Home() {
+  function handleParticipantAdd() {
+    console.log("Voce clicou no botao de adicionar!");
+  }
+
+  function handleParticipantRemove() {
+    console.log("Clicou em remover o participante: ")
+  }
+
   return (
     <View style={styles.container}>
-      <Text
-        style={styles.eventName}
-      >
-        Nome do evento
-      </Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
 
-      <Text
-        style={styles.eventDate}
-      >
-        Sexta, 30 de Dezembro 2022.
-      </Text>
+      <Text style={styles.eventDate}>Sexta, 30 de Dezembro 2022.</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant name="Vitor de Paula" onRemove={handleParticipantRemove}/>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -16,4 +16,32 @@ export const styles = StyleSheet.create({
     color: "#6b6b6b",
     fontSize: 16,
   },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42,
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+    color: '#FFF',
+    padding: 16,
+    fontSize: 16,
+    marginRight: 12,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31cf67',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24,
+  }
 });
